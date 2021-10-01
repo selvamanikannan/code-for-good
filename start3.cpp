d06302e10ad7a505517ac2499118224099fc5741
kselvamaniarkin <77379175+kselvamaniarkin@users.noreply.github.com>
2021-10-01 22:33:30 +0530

    add pattern printing more

---
+++ start3.cpp
@@ -0,0 +1,19 @@
+   //         * 
+   //       * * 
+   //     * * * 
+   //   * * * * 
+   // * * * * * 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n = 5;
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<n-i;j++)
+			cout<<"  ";
+		for(int j=n-i;j<n;j++)
+			cout<<"* ";
+		cout<<"\n";
+	}
+}
